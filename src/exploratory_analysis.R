dcadb56192e8952b2eb1d889de7b48cd2f13434d
Matheus Moura <matheus.moura@aluno.cefet-rj.br>
2024-09-17 08:32:04 -0300

    plot v1

--- src/exploratory_analysis.R
+++ src/exploratory_analysis.R
@@ -10,13 +10,21 @@ font <- theme(text = element_text(size=16))
 
 
 # Loading data set
-wine_df <- read.csv("data/wine.csv")
-head(wine_df)
-summary(wine_df)
+load("data/flights/bfd_2023.rdata")
+head(bfd)
+summary(bfd)
+
+# Delay bigger than threshold
+filtered_df <- bfd %>% filter(delay_depart > 5)
+head(filtered_df)
+summary(filtered_df)
+
+# Adding label
+bfd <- bfd %>% mutate(bool_delay = delay_depart > 5)
 
 
 # Density distribution
-wine_X_column_names <- colnames(wine_df)[2:length(wine_df)]
+X_column_names <- colnames(bfd)[1:length(bfd) - 1]
 create_plot_density_grf <- function(df, col_name, label_col_name) {
   num_unique_labels <- length(unique(df[[label_col_name]]))
   return(plot_density_class(df %>% dplyr::select(label_col_name, col_name), 
@@ -24,9 +32,9 @@ create_plot_density_grf <- function(df, col_name, label_col_name) {
                             color=colors[c(1:num_unique_labels)]) + font)
 }
 
-grfs <- purrr::map(wine_X_column_names,
-                   purrr::partial(create_plot_density_grf, df = wine_df,
-                                  label_col_name = "Type"))
+grfs <- purrr::map(X_column_names,
+                   purrr::partial(create_plot_density_grf, df = bfd,
+                                  label_col_name = "bool_delay"))
 
 options(repr.plot.width=8, repr.plot.height=8)
 do.call(grid.arrange, as.list(grfs))
