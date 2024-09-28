63c155cd4c768a31540d004dfdf23e84a22cad20
Matheus Moura <matheus.moura@aluno.cefet-rj.br>
2024-09-28 13:54:06 -0300

    adding feature encoding

--- src/main.R
+++ src/main.R
@@ -17,7 +17,26 @@ jk_flights <- get_flight_data(2023, "SBBR")
 # Pre-processing
 df <- jk_flights %>%
   drop_na() %>%
-  select(-flight, -expected_depart, -expected_arrival)
+  select(-c(expected_depart, expected_arrival, di, type, depart_visibility,
+            depart_pressure, depart_sky_coverage, depart_wind_direction_cat, flight))
+
+
+# Min-Max
+norm <- minmax()
+norm <- fit(norm, df)
+df <- transform(norm, df)
+summary(df)
+
+
+# Category mapping
+cat_cols_to_map <- c("arrival", "company", "depart_wind_speed_scale",
+                     "depart_day_period")
+for (col in cat_cols_to_map) {
+  cm <- categ_mapping(col)
+  aux_cm <- transform(cm, df)
+  df <- df %>% select(-one_of(col))
+  df <- cbind(df, aux_cm)
+}
 
 
 # Extracting levels
@@ -32,7 +51,7 @@ df_test <- sr$test
 
 
 # Model training
-model <- cla_rf("target", slevels)
+model <- cla_rf("target", slevels, mtry=8, ntree=128)
 model <- fit(model, df_train)
 train_prediction <- predict(model, df_train)
 
