3c6bba95871d06c9ff518abcf71c698935878cbe
Matheus Moura <matheus.moura@aluno.cefet-rj.br>
2024-09-30 22:17:13 -0300

    evaluation

--- src/main.R
+++ src/main.R
@@ -21,6 +21,7 @@ df <- jk_flights %>%
             depart_pressure, depart_sky_coverage, depart_wind_direction_cat, flight))
 
 
+
 # Min-Max
 norm <- minmax()
 norm <- fit(norm, df)
@@ -51,19 +52,44 @@ df_test <- sr$test
 
 
 # Model training
-model <- cla_rf("target", slevels, mtry=8, ntree=128)
+# model <- cla_knn("target", slevels, k=1)
+model <- cla_rf("target", slevels, mtry=3, ntree=5)
 model <- fit(model, df_train)
 train_prediction <- predict(model, df_train)
 
 
 # Model adjustment
-df_train_predictand <- adjust_class_label(df_train[,"target"])
-train_eval <- evaluate(model, df_train_predictand, train_prediction)
-print(train_eval$metrics)
+# df_train_predictand <- adjust_class_label(df_train[,"target"])
+# train_eval <- evaluate(model, df_train_predictand, train_prediction)
+# print(train_eval$metrics)
 
 
 # Model testing
-test_prediction <- predict(model, df_test)
-df_test_predictand <- adjust_class_label(df_test[,"target"])
-test_eval <- evaluate(model, df_test_predictand, test_prediction)
-print(test_eval$metrics)
+test_prediction <- predict(model, df_test)[,2]
+df_test_predictand <- adjust_class_label(df_test[,"target"])[,2]
+# test_eval <- evaluate(model, df_test_predictand, test_prediction)
+# print(test_eval$metrics)
+
+confusion_matrix <- table(df_test_predictand, test_prediction)
+
+# Accuracy
+accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+
+# Precision
+precision <- confusion_matrix[2, 2] / sum(confusion_matrix[, 2])
+
+# Recall (Sensitivity)
+recall <- confusion_matrix[2, 2] / sum(confusion_matrix[2, ])
+
+# F1 Score
+f1_score <- 2 * (precision * recall) / (precision + recall)
+
+# Results
+results <- data.frame(
+  Accuracy = accuracy,
+  Precision = precision,
+  Recall = recall,
+  F1_Score = f1_score
+)
+
+results
